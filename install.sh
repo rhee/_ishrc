5e6a72fba92d4ce6a9134cfe44009148016e30c6
Sang-Hoon RHEE <shr386+dev@hotmail.com>
2016-05-10 16:16:54 +0900

    .

--- install.sh
+++ install.sh
@@ -10,12 +10,21 @@ for f in _*; do
 	destfile=".$basename"
 	if [ -s "$f" ]; then
 	  if [ -z "$host" ]; then
+
 	    #( test ! -s "$HOME"/"$destfile" || \
 	    #  test "$f" -nt "$HOME"/"$destfile" ) && \
 	    #( echo cp "$f" "$HOME"/"$destfile" && \cp -p "$f" "$HOME"/"$destfile" )
+
+	    # quick hack create backup ( ~ )
+	    rsync -tP --backup "$f" "$HOME"/"$destfile"
+
+	    # delete rsync'd file, and then make symlink
 	    ln -v -sf "$PWD"/"$f" "$HOME"/"$destfile"
+
 	  else
+
 	    rsync -tP --backup "$f" "$host:$destfile"
+
 	  fi
 	fi
     ;;
